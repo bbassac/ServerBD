f1f96e3564d5c05a66f41f20175e39cc6e155ee2
Bruno Bassac <b.bassac@lectra.com>
2016-05-24 16:40:55 +0200

    introduced documentation

--- src/main/java/hello/Start.java
+++ src/main/java/hello/Start.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJSONDoc
 public class Start {
 
     public static void main(String[] args) throws Exception {
