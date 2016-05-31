e9ceb6fa62cbb9637cc9dd05ad279f284d254279
Bruno Bassac <b.bassac@lectra.com>
2016-05-31 10:49:00 +0200

    updated Webapp and do clean at startup

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,10 @@
 angular.module('myApp', [])
     .controller('home', function($scope,$http) {
-        //$scope.collection = {id: 'xxx', content: 'Hello World!'}
+        $scope.toggleDetail = function($index) {
+            //$scope.isVisible = $scope.isVisible == 0 ? true : false;
+            $scope.activePosition = $scope.activePosition == $index ? -1 : $index;
+        };
+
         $http.get('/collection').
         success(function(data) {
             $scope.collection = data;
