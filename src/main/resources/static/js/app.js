db307aa770d85c1ca9e5c130c2869cf23176c454
Bruno Bassac <b.bassac@lectra.com>
2016-09-29 16:30:05 +0200

    introduced order by fini

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,6 @@
 angular.module('myApp', [])
     .controller('home', function($scope,$http) {
+        $scope.sortReverse  = false;
         $scope.toggleDetail = function($index) {
             $scope.activePosition = $scope.activePosition == $index ? -1 : $index;
         };
