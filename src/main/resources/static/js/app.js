cf7f013cf83f769faa7950da99be3b3f2e09ce32
Bruno Bassac <b.bassac@lectra.com>
2016-09-29 16:44:18 +0200

    introduced order

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,7 @@
 angular.module('myApp', [])
     .controller('home', function($scope,$http) {
         $scope.sortReverse  = false;
+        $scope.sortType  = 'nom';
         $scope.toggleDetail = function($index) {
             $scope.activePosition = $scope.activePosition == $index ? -1 : $index;
         };
