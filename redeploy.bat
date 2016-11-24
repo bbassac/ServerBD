a4e6b42b71b0a4e7d38cea9c5ccc8afdbdb153e9
Bruno Bassac <b.bassac@lectra.com>
2016-11-24 14:57:04 +0100

    externalized mysql server

--- redeploy.bat
+++ redeploy.bat
@@ -2,4 +2,4 @@
 cls
 call git pull
 call mvn clean package
-call java -jar target/server-bd-1.0.jar
+call java -Dspring.datasource.url=jdbc:mysql://localhost:3306/collectionbd?useSSL=false -jar target\server-bd-1.0.jar
