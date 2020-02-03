3eb5def9938494db7ec0159aa968803ce330dad3
Bassac Bruno <b.bassac@lectra.com>
2020-02-03 15:53:26 +0100

    made responsive

--- redeploy.bat
+++ redeploy.bat
@@ -1,5 +1,5 @@
 
 cls
 call git pull
-call mvn clean package
+call mvn clean package -DskipTests
 call java -Dspring.datasource.url=jdbc:mysql://localhost:3306/collectionbd?useSSL=false -jar target\server-bd-1.0.jar
